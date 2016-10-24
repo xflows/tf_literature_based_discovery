b0a5d7a6dcf43fd49d8e44c182a6651d7f4b9eb8
Matic Perovšek <mperice@gmail.com>
2016-10-24 14:36:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# TextFlows Literature Based Discovery Module #
+
+
+A [TextFlows](https://github.com/xflows/textflows/) package which text mining widgets (UI components) for literature based cross-domain discovery. The package can also be used with [ClowdFlows](https://github.com/xflows/clowdflows/) 2.0.
+
+[![Documentation Status](https://readthedocs.org/projects/rdm/badge/?version=latest)](http://docs.textflows.org/)
+
+
+## Installation, documentation ##
+
+Please find installation instructions, examples and API reference on [Read the Docs](http://docs.textflows.org/).
+
+## Note ##
+
+Please note that this is a research project and that drastic changes can be (and are) made pretty regularly. Changes are documented in the [CHANGELOG](CHANGELOG.md).
+
+Pull requests and issues are welcome.
+
+## Contributors to the tf_literature_based_discovery package code ##
+
+Matic Perovšek (@mperice), Matjaž Juršič (@mjursic)
+
+* [Knowldge Technologies Department](http://kt.ijs.si), Jožef Stefan Institute, Ljubljana
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tf_literature_based_discovery',
+    version='0.0.1',
+    packages=['tf_literature_based_discovery'],
+    include_package_data=True,
+    license='MIT License',
+    description='TextFlows module for literature-based cross-context discovery',
+    long_description=README,
+    url='https://github.com/xflows/tf_literature_based_discovery',
+    author='Matic Perovsek',
+    author_email='matic.perovsek@ijs.si',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Prolog',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
